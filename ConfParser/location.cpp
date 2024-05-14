bc7a9c5475e60c72eeaad59644a5403d9c3c122f
LoutfiDev <soulang.dev@gmail.com>
2024-05-14 12:55:38 +0100

    still parsin !! working on Location block, allowed_methods directive parsed

--- ConfParser/location.cpp
+++ ConfParser/location.cpp
@@ -6,7 +6,7 @@
 /*   By: soulang <soulang@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 10:20:30 by soulang           #+#    #+#             */
-/*   Updated: 2024/05/14 11:14:11 by soulang          ###   ########.fr       */
+/*   Updated: 2024/05/14 12:54:12 by soulang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,7 +62,7 @@ int parse_value(std::string value)
 	else if (value == "off")
 		return (0);
 	else
-		throw 60;
+		throw 4;
 }
 
 void Location::set_auto_index(std::string& rest){
@@ -80,8 +80,6 @@ void Location::set_auto_index(std::string& rest){
 		}
 		std::stringstream s(rest);
 		std::getline(s, value, ';');
-		if (value.empty())
-			throw 70;
 		rest.erase(0, value.size());
 		std::stringstream ss(value);
 		while (std::getline(ss, value, ' '))
@@ -91,22 +89,65 @@ void Location::set_auto_index(std::string& rest){
 			else if (nb < 1)
 				nb = parse_value(value);
 			else
-				throw 40;
+				throw 2;
 		}
 	}
 	if (nb == -1)
-		throw 50;
+		throw 3;
 	autoindex = nb;
 }
 
 void Location::set_root(std::string& rest){
-	(void)rest;
-}
-	// std::cout << "hello" << std::endl;
+	// 		std::cout << "hello" << std::endl;
 	// std::cout << "|" << rest << "|" << std::endl;
 	// exit(0);
+}
+
+std::string parse_methods(std::vector<std::string>v, std::string value)
+{
+	std::string str[3] = {"post", "get", "delete"};
+	std::string::iterator it = value.begin();
+	for(; it != value.end(); ++it) { *it =  std::tolower(*it); }
+	std::vector<std::string>::iterator ite = v.begin();
+	for (; ite != v.end(); ++ite)
+	{
+		if (value == *ite)
+			throw 55;
+	}
+	for (int i = 0; i < 3; i++)
+	{
+		if (str[i] == value)
+			return (value);
+	}
+	throw 3;
+}
+
 void Location::set_allow_methods(std::string& rest){
-	
+	std::string value;
+	    
+	while (!rest.empty())
+	{
+		while (rest[0] == ' ')
+			rest.erase(0, 1);
+		if (rest[0] == ';')
+		{
+			rest.erase(0, 1);
+			break;
+		}
+		std::stringstream s(rest);
+		std::getline(s, value, ';');
+		rest.erase(0, value.size());
+		std::stringstream ss(value);
+		while (std::getline(ss, value, ' '))
+		{
+			if (value.empty())
+				continue;
+			else
+				allow_methods.push_back(parse_methods(allow_methods, value));
+		}
+	}
+	if (allow_methods.size() == 0)
+		throw 66;
 }
 void Location::set_index(std::string& rest){
 	(void)rest;
